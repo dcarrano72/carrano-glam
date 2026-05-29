e1adc551f207068286357717d3bc4992adb3a677
Dave Carrano <dcarrano72@gmail.com>
2026-05-29 14:27:53 -0400

    updated eleventy.js to allow upload of sitemap and robots

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addWatchTarget("src/assets");
+  eleventyConfig.addPassthroughCopy("src/sitemap.xml");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
   return {
     dir: {
       input: "src",
